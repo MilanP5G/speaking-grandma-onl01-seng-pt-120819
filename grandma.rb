5a871d09b48548b76fe622ed35abd57a68f330c7
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-11 16:23:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else phrase == phrase.capitalize
+  else 
     return "HUH?! SPEAK UP, SONNY!"
   end
   
